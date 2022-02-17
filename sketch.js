be3e3251b748ec43258f1b14d940682a544c7e60
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2022-02-17 12:57:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,6 +17,17 @@ function setup() {
 	world = engine.world;
 
 
+	bob1=new Bob(100,480,25);
+	bob2=new Bob(150,485,25);
+	bob3=new Bob(200,490,25);
+	bob4=new Bob(250,495,25);
+	bob5=new Bob(300,500,25);
+
+	rope1=new Rope(bob1.body,roof.body,-100)
+	rope2=new Rope(bob2.body,roof.body,-50)
+	rope3=new Rope(bob3.body,roof.body,0)
+	rope4=new Rope(bob4.body,roof.body,50)
+	rope5=new Rope(bob5.body,roof.body,100)
 
   Engine.run(engine);
 }
@@ -25,7 +36,21 @@ function setup() {
 function draw() {
 
   background(0);
-  
+	
+  roof.display();
+	
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
 
 
  
